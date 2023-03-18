3dd85423d69f988a0285f5707274a2f79da25a75
ZGuyDev <ZGuyDev@users.noreply.github.com>
2023-03-19 01:12:24 +0300

    added img resolve

--- restapi/__resources.py
+++ restapi/__resources.py
@@ -10,3 +10,7 @@ def js_resource(script_name) -> Response:
 @bp.route('/css/<style_name>')
 def css_resource(style_name) -> Response:
     return send_from_directory(os.path.join(bp.static_folder, "css"), style_name)
+
+@bp.route('/img/<img_name>')
+def css_resource(img_name) -> Response:
+    return send_from_directory(os.path.join(bp.static_folder, "img"), img_name)
